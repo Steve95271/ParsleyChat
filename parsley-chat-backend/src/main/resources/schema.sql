528dd3d19f23c699b91febb16c431a30e25263e6
Steve95271 <124816688+Steve95271@users.noreply.github.com>
2025-11-29 12:41:21 +0800

    Update create table sql suitable for Postgres

--- parsley-chat-backend/src/main/resources/schema.sql
+++ parsley-chat-backend/src/main/resources/schema.sql
@@ -1,38 +1,42 @@
-CREATE TABLE IF NOT EXISTS user
+DROP TABLE IF EXISTS "users";
+CREATE TABLE IF NOT EXISTS "users"
 (
-    id       BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-    username VARCHAR(128)    NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id        BIGINT NOT NULL PRIMARY KEY,
+    username  VARCHAR(128) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS room
+DROP TABLE IF EXISTS rooms;
+CREATE TABLE IF NOT EXISTS rooms
 (
-    id         BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-    name       VARCHAR(128)    NOT NULL,
-    creator_id BIGINT UNSIGNED NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id         BIGINT NOT NULL PRIMARY KEY,
+    name       VARCHAR(128) NOT NULL,
+    creator_id BIGINT NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS message
+DROP TABLE IF EXISTS messages;
+CREATE TABLE IF NOT EXISTS messages
 (
-    id          BIGINT UNSIGNED NOT NULL PRIMARY KEY,
-    room_id     BIGINT UNSIGNED NOT NULL,
-    sender_id   BIGINT UNSIGNED NOT NULL,
-    sender_name VARCHAR(128),
-    content     VARCHAR(4000),
-    time_stamp  DATETIME DEFAULT CURRENT_TIMESTAMP
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    room_id      BIGINT NOT NULL,
+    sender_id    BIGINT NOT NULL,
+    sender_name  VARCHAR(128),
+    content      VARCHAR(4000),
+    time_stamp   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE IF NOT EXISTS room_member
+-- In PostgresSQL, we need to create ENUM type first
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'room_role') THEN
+CREATE TYPE room_role AS ENUM ('creator', 'admin', 'member');
+END IF;
+END$$;
+
+DROP TABLE IF EXISTS room_members;
+CREATE TABLE IF NOT EXISTS room_members
 (
-    room_id   BIGINT UNSIGNED NOT NULL,
-    user_id   BIGINT UNSIGNED NOT NULL,
-    joined_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    role      ENUM ('creator', 'admin', 'member')
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+    room_id   BIGINT NOT NULL,
+    user_id   BIGINT NOT NULL,
+    joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    role      room_role
+);
